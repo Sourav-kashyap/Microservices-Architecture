0cadc30f6f015bd8b2c34e41d1ea5ced38979578
Sourav-kashyap <sourav.kashyap@sourcefuse.com>
2025-04-03 14:14:34 +0530

    Resolved bug in get current user endpoint.

--- user/src/controllers/user.controller.ts
+++ user/src/controllers/user.controller.ts
@@ -1,29 +1,29 @@
-import {authenticate} from 'loopback4-authentication';
-import {Filter, repository} from '@loopback/repository';
-import {get, HttpErrors, param} from '@loopback/rest';
+import {authenticate, AuthenticationBindings} from 'loopback4-authentication';
+import {repository, Getter} from '@loopback/repository';
+import {get, HttpErrors} from '@loopback/rest';
 import {STRATEGY} from 'loopback4-authentication';
 import {User} from '../models/user.model';
 import {UserRepository} from '../repositories/user.repository';
+import {inject} from '@loopback/core';
 
 export class UserController {
   constructor(
     @repository(UserRepository)
     public userRepository: UserRepository,
+    @inject.getter(AuthenticationBindings.CURRENT_USER)
+    private readonly getAuthenticateUser: Getter<User>,
   ) {}
 
   @authenticate(STRATEGY.BEARER)
   @get('/users')
-  async find(
-    @param.query.object('filter') filter?: Filter<User>,
-  ): Promise<User[]> {
-    console.log('/users ->', filter);
-    return this.userRepository.find(filter);
+  async find(): Promise<User[]> {
+    return this.userRepository.find();
   }
 
   @authenticate(STRATEGY.BEARER)
   @get('/me')
   async getCurrentUser(): Promise<User> {
-    const user = await this.getCurrentUser();
+    const user = await this.getAuthenticateUser();
     if (!user) {
       throw new HttpErrors.Unauthorized('No authenticated user found');
     }
