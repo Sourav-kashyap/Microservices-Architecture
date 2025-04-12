9b354da11e2e592878a7da2e6ea9995965a58d2f
Sourav-kashyap <sourav.kashyap@sourcefuse.com>
2025-04-12 20:25:33 +0530

    Resolve some minor bugs related to authentication.

--- user/src/application.ts
+++ user/src/application.ts
@@ -9,7 +9,6 @@ import {RestApplication} from '@loopback/rest';
 import {ServiceMixin} from '@loopback/service-proxy';
 import path from 'path';
 import {MySequence} from './sequence';
-import {AuthenticationComponent} from 'loopback4-authentication';
 
 export {ApplicationConfig};
 
@@ -18,8 +17,6 @@ export class UserApplication extends BootMixin(
 ) {
   constructor(options: ApplicationConfig = {}) {
     super(options);
-
-    this.component(AuthenticationComponent);
 
     // Set up the custom sequence
     this.sequence(MySequence);
--- user/src/sequence.ts
+++ user/src/sequence.ts
@@ -9,8 +9,6 @@ import {
   SequenceActions,
   SequenceHandler,
 } from '@loopback/rest';
-import {AuthenticateFn, AuthenticationBindings} from 'loopback4-authentication';
-import {User} from './models';
 export class MySequence implements SequenceHandler {
   constructor(
     @inject(SequenceActions.FIND_ROUTE) protected findRoute: FindRoute,
@@ -18,8 +16,6 @@ export class MySequence implements SequenceHandler {
     @inject(SequenceActions.INVOKE_METHOD) protected invoke: InvokeMethod,
     @inject(SequenceActions.SEND) public send: Send,
     @inject(SequenceActions.REJECT) public reject: Reject,
-    @inject(AuthenticationBindings.USER_AUTH_ACTION)
-    protected authenticateRequest: AuthenticateFn<User>,
   ) {}
   async handle(context: RequestContext) {
     try {
@@ -27,8 +23,6 @@ export class MySequence implements SequenceHandler {
       const route = this.findRoute(request);
       const args = await this.parseParams(request, route);
       request.body = args[args.length - 1];
-      // Authenticate the request
-      // const authUser: User = await this.authenticateRequest(request);
       const result = await this.invoke(route, args);
       this.send(response, result);
     } catch (err) {
